36b9cfc50cb2f2c086f332276b4934d76ab758db
Santiagoymn <91144634+Santiagoymn@users.noreply.github.com>
2022-04-13 12:57:26 +0100

    Sprint 3. Activity page con todo

--- src/app/activity-page-information/activity-page-information.component.ts
+++ src/app/activity-page-information/activity-page-information.component.ts
@@ -1,29 +1,44 @@
-// import {Component, Input, OnInit} from '@angular/core';
-// import { ActivatedRoute } from '@angular/router';
-// import {Categoria, Actividad, Empresa, DiscoverGC} from "../objetos";
-// import { CategoriaService } from '../categoria.service';
-//
-// @Component({
-//   selector: 'app-activity-page-information',
-//   templateUrl: './activity-page-information.component.html',
-//   styleUrls: ['./activity-page-information.component.css']
-// })
-// export class ActivityPageInformationComponent implements OnInit {
-//
-//   product: Product | undefined;
-//
-//   data : any;
-//   categorias!: Categoria[];
-//   @Input() categoria!: Categoria;
-//
-//   constructor(private route: ActivatedRoute, private categoriaService: CategoriaService) {
-//
-//   }
-//
-//   ngOnInit(): void {
-//     const routeParams = this.route.snapshot.paramMap;
-//     const activityIdFromRoute = Number(routeParams.get('activityId'));
-//     this.product = this.categoriaService.getListCategorias().find(product => product.id === activityIdFromRoute) ;
-//   }
-//
-// }
+import {Component, Input, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Categoria, Actividad, Empresa, DiscoverGC} from "../objetos";
+import { CategoriaService } from '../categoria.service';
+import {GetterJsonService} from "../getter-json.service";
+import {tap} from "rxjs";
+
+@Component({
+  selector: 'app-activity-page-information',
+  templateUrl: './activity-page-information.component.html',
+  styleUrls: ['./activity-page-information.component.css']
+})
+export class ActivityPageInformationComponent implements OnInit {
+
+  categorias!: Categoria[];
+  @Input() categoria!: Categoria;
+  constructor(private getterJsonService: GetterJsonService) {
+  }
+
+  ngOnInit(): void {
+    this.getterJsonService.getCategorias()
+      .pipe(
+        tap((categories: Categoria[]) => this.categorias = categories)
+      )
+      .subscribe();
+  }
+
+  // product: Product | undefined;
+  //
+  // data : any;
+  // categorias!: Categoria[];
+  // @Input() categoria!: Categoria;
+  //
+  // constructor(private route: ActivatedRoute, private categoriaService: CategoriaService) {
+  //
+  // }
+  //
+  // ngOnInit(): void {
+  //   const routeParams = this.route.snapshot.paramMap;
+  //   const activityIdFromRoute = Number(routeParams.get('activityId'));
+  //   this.product = this.categoriaService.getListCategorias().find(product => product.id === activityIdFromRoute) ;
+  // }
+
+}
